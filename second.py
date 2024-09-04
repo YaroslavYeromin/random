08b64ea21a8b7459bba20364951056e31c3a60d5
123 <eremin2017yt@gmail.com>
2024-09-04 16:22:31 +0200

    first commit

--- second.py
+++ second.py
@@ -1,21 +1,22 @@
 from datetime import datetime
 
-def get_days_from_today(date) :
-    
-    
-    year = 2023
-    mounth = 9
-    day = 1
-    if mounth > 12 or day > 31 or year < 0:
-        return "некоректный ввод"
-              
-    date = datetime(year , mounth , day ).date()
+def days_difference(date):
+   
+    try:
+        input_date = datetime.strptime(date, '%Y-%m-%d').date()
+    except ValueError:
+        return ("Некореткный ввод")
         
-    date_today = datetime.today().date()
+    current_date = datetime.today().date()
+    
+    
+    difference = current_date - input_date
+    
   
-    delta_date = date_today - date
-    
-    
-    return delta_date.days
-days_dif =get_days_from_today("2020.12.12")
-print(days_dif)
+    return difference.days
+
+
+
+date = "2020-10-32"
+
+print(days_difference(date))
